012a457145d248747ad92b8ed8d922ecd2390238
Daniel <ggsan.com@gmail.com>
2025-03-12 16:22:57 +0800

    friendly neighbour implementation

---
+++ testall.cmd
@@ -0,0 +1,9 @@
+python vrp.py < example_testcases/1.in > 1.out
+timeout 1
+python vrp.py < example_testcases/2.in > 2.out
+timeout 1
+python vrp.py < example_testcases/3.in > 3.out
+timeout 1
+python vrp.py < example_testcases/4.in > 4.out
+timeout 1
+python vrp.py < example_testcases/5.in > 5.out
--- vrp.py
+++ vrp.py
@@ -24,30 +24,66 @@ def read_input():
 
 def solve_cvrp(n, Q, D, q):
     """TODO: Solve the Capacitated Vehicle Routing Problem and return a list of routes."""
-    routes = [[0]]
-    
+    depot_distance = D[0]
+    routes = []
+    curr_route = [0]
+    curr_cap = 0
+    visited = set([0])
+
+    def get_closest_index(array: list):
+        closest = None
+        for i, dist in enumerate(array):
+            if i in visited:
+                continue
+
+            if dist > 0 and closest is None:
+                closest = i
+                continue
+            
+            if dist > 0 and dist < array[closest]:
+                closest = i
+
+        return closest
+
+    for i in range(n-1):
+        target = get_closest_index(D[i])
+        if curr_cap + q[target] > Q:
+            curr_route.append(0)
+            routes.append(curr_route)
+            curr_route = [0]
+            curr_cap = 0
+        curr_route.append(target)
+        curr_cap += q[target]
+        visited.add(target)
+
+    # capture last trip
+    curr_route.append(0)
+    routes.append(curr_route)
     return routes
 
-def check(routes, n, Q, D, q):
-    node_visited = []
-    for route in routes:
-        total_demand = sum([q[i] for i in route if i != 0])
-        if not (total_demand <= Q):
-            return False
-        node_visited += route
+# def check(routes, n, Q, D, q):
+#     node_visited = []
+#     for route in routes:
+#         total_demand = sum([q[i] for i in route if i != 0])
+#         if not (total_demand <= Q):
+#             return False
+#         node_visited += route
 
-    if len(set(node_visited)) != len(q):
-        return False
+#     if len(set(node_visited)) != len(q):
+#         return False
 
-    return True
+#     return True
 
 def main():
     n, Q, D, q = read_input()
     routes = solve_cvrp(n, Q, D, q)
 
-    if check(routes, n, Q, D, q): 
-        for route in routes:
-            print(" ".join(map(str, route)))
+    # if check(routes, n, Q, D, q): 
+    #     for route in routes:
+    #         print(" ".join(map(str, route)))
+
+    for route in routes:
+        print(" ".join(map(str, route)))
 
 if __name__ == "__main__":
     main()
